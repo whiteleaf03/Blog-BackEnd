0aa20fe04df3f2545c8e741cc975d730b513d92e
WhiteLeaf03 <2876202234@qq.com>
2022-10-18 16:19:24 +0800

    防止类名冲突

--- src/main/java/cn/whiteleaf03/blogbackend/BlogBackEndApplication.java
+++ src/main/java/cn/whiteleaf03/blogbackend/BlogBackEndApplication.java
@@ -1,9 +1,15 @@
 package cn.whiteleaf03.blogbackend;
 
+import cn.whiteleaf03.blogbackend.config.BeanNameConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * @author WhiteLeaf03
+ */
 @SpringBootApplication
+@ComponentScan(nameGenerator = BeanNameConfig.class)
 public class BlogBackEndApplication {
 
     public static void main(String[] args) {
